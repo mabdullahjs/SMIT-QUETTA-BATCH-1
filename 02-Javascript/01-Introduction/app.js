b4971b82782becfa2e39ca2d1ccdee0f639dd7ab
mabdullahjs <mabdullah2037@outlook.com>
2025-05-17 13:18:28 +0500

    javascript started

---
+++ 02-Javascript/01-Introduction/app.js
@@ -0,0 +1,96 @@
+/* console.log("Hello world");
+console.log("Hello Javascript"); */
+
+// function
+
+
+// alert("hello Abdullah")
+// alert("hello Abdullah")
+// alert("hello Abdullah")
+
+
+
+
+
+// Data types
+
+// string "" ''
+// number 12.234
+// boolean true / false
+
+
+// object
+// bigint
+// null
+// undefined
+// symbol
+
+
+
+// console.log("125@#$23#%@#sdgsgasg"); // string
+// console.log(125);  // number
+// console.log(true) // boolean
+// console.log(false) // boolean
+
+
+// console.log("123");
+// console.log(123);
+
+
+
+
+
+
+// var username = "Muhammad Abdullah";
+// var email = "mabdullah2037@gmail.com";
+// var age = 21;
+
+// console.log(username)
+// console.log(email)
+// console.log(age)
+
+
+
+
+// var city = "karachi"; // declare // assign 
+// var age = 21 // initialize | declare | assign
+
+
+
+// var username = "Muhammad abdullah"; // initialize
+// var username = "Usman"; // redeclare
+// username = "Ammar" // reassign
+
+
+
+
+// var city = "karachi";
+// console.log(city)
+// city = "Quetta";
+// console.log(city)
+
+
+
+// initialize
+// declare
+// assign
+// redeclare
+// reassign
+
+// var $_username123 = "karachi"
+// var fullName123 = "abdullah"
+// console.log(fullName123);
+
+
+
+
+
+
+// concatinate
+// variable names
+
+
+
+
+
+
