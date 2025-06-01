17d91de4022cabe4a33907513da4c59a111616d2
mabdullahjs <mabdullah2037@outlook.com>
2025-06-01 10:59:36 +0500

    dom manipulation

---
+++ 02-Javascript/05-DOM-manipulation/app.js
@@ -0,0 +1,57 @@
+// console.log("hello world");
+
+
+// var heading = document.querySelector("#heading")
+// var paragraph = document.querySelector("#para");
+
+
+// console.log(heading.innerHTML);
+
+// heading.innerHTML = "Changed through Javascript"
+
+// paragraph.innerHTML = "change this paragraph using javascript"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var h1 = document.querySelector('#heading');
+// var num = 0
+
+// function changetext(){
+//     // console.log("text changed");
+//     num += 1
+//     h1.innerHTML = "changed through javascript " + num
+// }
+
+
+
+
+
+
+// var h2 = document.querySelector("#heading-two")
+
+// function changedText(){
+//     console.log("hello world" , h2.innerHTML);
+//     if(h2.innerHTML === "available"){
+//         h2.innerHTML = "not available"
+//     }else {
+//         h2.innerHTML = "available"
+
+//     }
+    
+// }
+
+
+
