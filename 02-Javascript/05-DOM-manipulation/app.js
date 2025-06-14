84ef6e1d590ea98f159110c02f1fe1ae56fe0d91
mabdullahjs <mabdullah2037@outlook.com>
2025-06-14 09:54:54 +0500

    dom manipulation

--- 02-Javascript/05-DOM-manipulation/app.js
+++ 02-Javascript/05-DOM-manipulation/app.js
@@ -1,30 +1,13 @@
 // console.log("hello world");
 
-
 // var heading = document.querySelector("#heading")
 // var paragraph = document.querySelector("#para");
-
 
 // console.log(heading.innerHTML);
 
 // heading.innerHTML = "Changed through Javascript"
 
 // paragraph.innerHTML = "change this paragraph using javascript"
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 // var h1 = document.querySelector('#heading');
 // var num = 0
@@ -34,11 +17,6 @@
 //     num += 1
 //     h1.innerHTML = "changed through javascript " + num
 // }
-
-
-
-
-
 
 // var h2 = document.querySelector("#heading-two")
 
@@ -50,8 +28,48 @@
 //         h2.innerHTML = "available"
 
 //     }
-    
+
+// }
+
+// var h1 = document.querySelector("#heading-one");
+// function changeText(){
+//     console.log("text changed");
+//     console.log(h1.innerHTML);
+//     h1.innerHTML = "Changed through Javascript"
+//     console.log(h1.innerHTML);
+
+// }
+
+// var counterHeading = document.querySelector("#counter-heading");
+// var counter = 0;
+
+// function addCounter() {
+//   counter += 1;
+//   console.log("counter add", counter);
+//   counterHeading.innerHTML = counter;
+// }
+// function lessCounter() {
+//   if (counter < 1) {
+//     alert("akhir krna kiya chahta ho");
+//   } else {
+//     counter -= 1;
+//     console.log("counter less");
+//     counterHeading.innerHTML = counter;
+//   }
 // }
 
 
 
+// var para = document.querySelector(".message");
+// para.innerHTML = "welcome!"
+
+
+var input = document.querySelector('#nameInput');
+var result = document.querySelector("#result");
+
+// console.log("outside func" , input.value);
+function getuserVal(){
+    console.log("inside func" , input.value);
+    result.innerHTML = "hello " + input.value
+    
+}
