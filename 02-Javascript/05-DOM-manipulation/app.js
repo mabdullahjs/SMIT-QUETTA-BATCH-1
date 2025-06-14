2294323ca209c88ef246caf08a2427571789374f
mabdullahjs <mabdullah2037@outlook.com>
2025-06-14 10:44:55 +0500

    styling with js

--- 02-Javascript/05-DOM-manipulation/app.js
+++ 02-Javascript/05-DOM-manipulation/app.js
@@ -64,12 +64,66 @@
 // para.innerHTML = "welcome!"
 
 
-var input = document.querySelector('#nameInput');
-var result = document.querySelector("#result");
+// var input = document.querySelector('#nameInput');
+// var result = document.querySelector("#result");
 
-// console.log("outside func" , input.value);
-function getuserVal(){
-    console.log("inside func" , input.value);
-    result.innerHTML = "hello " + input.value
+// // console.log("outside func" , input.value);
+// function getuserVal(){
+//     console.log("inside func" , input.value);
+//     result.innerHTML = "hello " + input.value
+    
+// }
+
+
+// var num1 = document.querySelector("#num1")
+// var num2 = document.querySelector("#num2")
+// var result = document.querySelector("#result")
+
+// function total(){
+//     var sum = +num1.value + +num2.value
+//     result.innerHTML = "the sum of " + num1.value + " and " + num2.value + " is " + sum;
+//     result.style.color = "white"
+//     result.style.backgroundColor = "black"
+
+// }
+
+
+
+var body = document.querySelector("body");
+
+// body.style.backgroundColor = "red"
+
+function changeColor(color){
+    console.log(color);
+    body.style.backgroundColor = color
     
 }
+
+var image = document.querySelector("#image");
+
+function imageChange(){
+    image.src = "https://images.unsplash.com/photo-1746748694015-036d4dd23c13?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+}
+
+var para = document.querySelector("#para");
+var btn = document.querySelector("#btn");
+
+function togglePara(){
+    // console.log(btn.innerHTML);
+    
+    console.log(para.innerHTML);
+    if(para.style.visibility == "hidden"){
+        para.style.visibility = "visible"
+        btn.innerHTML = "toggle off"
+    }else {
+        para.style.visibility = "hidden"
+        btn.innerHTML = "toggle on"
+    }
+    
+}
+
+
+// innerhtml ka through element ka content change krna
+// input form ki value get krna using .value
+// styling dena js ka thorugh using .style.cssproperty
+// image ko change krskta using .src
