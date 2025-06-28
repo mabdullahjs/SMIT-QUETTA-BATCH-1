e97155d2be1a3ba14c806fd04a88e252aae864f6
mabdullahjs <mabdullah2037@outlook.com>
2025-06-28 13:29:43 +0500

    loops started

---
+++ 02-Javascript/07-Loops/app.js
@@ -0,0 +1,60 @@
+// console.log("hello world");
+
+
+// let username = "Sir Abdullah";
+// let age = 20;
+// let email = "mabdullah@gmail.com";
+
+// // console.log("username is " + username + " and age is " + age + " and email is " + email + " " + age);
+// console.log(`hello world ${username} and age is ${age} and email is ${email}`);
+
+
+
+// for state
+
+
+// i = iteration
+
+// initialization condition increment
+
+// infinite loop
+
+// for (let i = 1; i <= 10; i++) {
+//     console.log(i);
+// }
+
+// let num = 10;
+
+
+// // 10 * 1 = 10
+// // 10 * 2 = 20      
+
+
+
+
+
+
+
+
+let num = 48;
+
+for(let i = 1; i <= 10; i++){
+    console.log(`${num} * ${i} = ${num * i}`);
+}
+
+
+
+
+
+
+// dom manipulation
+// nested loop
+// loops with arrays
+// while loop
+// do while loop
+
+
+
+
+
+
