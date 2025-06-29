47ccc1d6653ac9e956e5c7e5bc2b4c1150930059
mabdullahjs <mabdullah2037@outlook.com>
2025-06-29 10:16:31 +0500

    loops

--- 02-Javascript/07-Loops/app.js
+++ 02-Javascript/07-Loops/app.js
@@ -36,13 +36,58 @@
 
 
 
-let num = 48;
+// let num = 48;
 
-for(let i = 1; i <= 10; i++){
-    console.log(`${num} * ${i} = ${num * i}`);
-}
+// for(let i = 1; i <= 10; i++){
+//     console.log(`${num} * ${i} = ${num * i}`);
+// }
 
 
+
+// for(let i = 10; i >= 1; i--){
+//     console.log(i);
+    
+// }
+
+
+
+
+// nested loop
+
+// for(let i = 0; i < 5; i++){
+//     console.log("parent loop ==>" , i);
+//     for(let j = 0; j < 5; j++){
+//         console.log("child loop ==>" , j);
+//     }
+// }
+
+
+
+// for(let i = 1; i <= 1000; i++){
+//     console.log(`Table of ${i}`);
+//     for(let j = 1; j <= 10; j++){
+//         console.log(`${i} * ${j} = ${i * j}`);
+//     }
+//     console.log('--------------------');
+    
+    
+// }
+
+
+
+// const input = document.querySelector("#numberInput");
+// const result = document.querySelector("#result");
+
+
+// function generateTable(){
+//     const number = +input.value;
+//     for(let i = 1; i <= 10; i++){
+//         console.log(`${number} * ${i} = ${number * i}`);
+//         result.innerHTML += `<p>${number} * ${i} = ${number * i}</p>`; 
+        
+//     }
+    
+// }
 
 
 
@@ -55,6 +100,18 @@ for(let i = 1; i <= 10; i++){
 
 
 
+// const fruits = ["apple", "banana", "orange", "mango", "grape"];
 
+// for(let i = 0; i < fruits.length; i++){
+//     console.log(fruits[i] , i);
+    
+// }
 
+const result = document.querySelector("#result");
 
+const cities = ["Karachi", "Lahore", "Quetta", "Islamabad", "Peshawar"];
+
+for(let i = 0; i < cities.length; i++){
+    console.log(cities[i]);
+    result.innerHTML += `<p>${cities[i]}</p>`;
+}
