90efedb0987ecdceb72ba93642007322b31e94b2
mabdullahjs <mabdullah2037@outlook.com>
2025-07-19 17:34:24 +0500

    function

---
+++ 02-Javascript/04-Function-&-DOM/app.js
@@ -0,0 +1,93 @@
+// function greetuser() {
+//   console.log("hello world 1");
+//   console.log("hello world 2");
+//   console.log("hello world 3");
+//   console.log("hello world 4");
+//   console.log("hello world 5");
+// }
+
+// greetuser()
+// greetuser()
+
+
+
+
+
+
+
+// parameter | argument
+// function greetUser(username){
+//     console.log("hello " + username);
+// }
+
+// greetUser("abdullah")
+// greetUser("Usman")
+
+
+
+
+
+
+// return bilkul end ma ata ha
+// return ka baad kuch nahi likhna
+// return sirf aik dafa hoskta
+
+// function sum(num1 , num2){
+//     console.log(num1 + num2);
+//     return "num1 + num2"    
+// }
+
+// sum(20 , 30)
+// sum(80 , 20)
+
+
+
+// return 
+
+
+// var username = prompt("enter your username");
+
+// console.log(username);
+
+
+// var total = sum(20 , 30);
+// console.log(total);
+
+
+
+
+
+
+
+
+
+
+// function checkCity(city){
+//     return "hello from " + city
+// }
+
+// var greetuser = checkCity()
+// console.log(greetuser);
+
+
+
+
+
+// local variable
+// global variable
+
+
+var username = "abdullah"
+var age = 21
+
+function greetUser(fullname){
+    var lastName = "khan" // local varibale
+    console.log("hello " + fullname + " " + lastName);
+    console.log(age);
+    
+}
+
+
+greetUser(username)
+
+
