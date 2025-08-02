685948659c80e51ee83f1c877a4d79d689ec2819
mabdullahjs <mabdullah2037@outlook.com>
2025-08-02 18:12:33 +0500

    loop started

--- 02-Javascript/05-Array/app.js
+++ 02-Javascript/05-Array/app.js
@@ -1,28 +1,19 @@
 // var , let , const
 
-
 // var username = "abdullah"
 // username = "usman" // reassign
 // var username = "Ammar" // redeclare
 
 // reassign + redeclare
 
-
 // let username = "Abdullah"
 // username = "Usman"
 // let username = "ammar"
-// reassign 
+// reassign
 
 // const username = "Abdullah"
 
-
 // console.log(username);
-
-
-
-
-
-
 
 // index 0
 // length 1
@@ -30,10 +21,6 @@
 // let names = ["abdullah" , "usman" , "ammar"]
 // // console.log(names[4]);
 // console.log(names);
-
-
-
-
 
 //push
 //pop
@@ -48,13 +35,20 @@
 //reverse
 //indexof
 
+let fruits = ["apple" , "banana" , "orange" , "pineapple" , "watermelon" , "melon" , "mango" , "peach"];
 
+// console.log(fruits[1]);
+// fruits[1] = "updated"
 
-let fruits = ["apple" , "banana" , "orange" , "pineapple"];
+// fruits.splice(1 , 2 , "watermelon")
+// fruits.splice(2 , 0 , "item added" , "item added 2")
+// console.log(fruits);
 
-fruits[1] = "updated"
-console.log(fruits[1]);
+// let copy = fruits.slice(1 , 5)
+// console.log(copy);
 
+// fruits[1] = "updated"
+// console.log(fruits[1]);
 
 // fruits.push("mango")
 // fruits.pop()
@@ -62,8 +56,53 @@ console.log(fruits[1]);
 // fruits.shift()
 // console.log(fruits);
 
-
 // fruits.splice(1 , 0 , "watermelon")
 // fruits.splice(2 , 1 , "kiwi" , "updated")
 // fruits.splice(3 , 1)
-console.log(fruits);
+// console.log(fruits);
+
+// let username = "Muhammad Abdullah Khan"
+
+// let convertedArr = username.split()
+// console.log(fruits.join(", "));
+
+// string ko reverse krdo
+
+// let city = "Kiraaaanchi"
+// console.log(city.split('').reverse().join(""));
+
+// function reverseString(str){
+//     return str.split('').reverse().join("")
+// }
+
+// console.log(reverseString("level"));
+
+// const input = document.querySelector("#input");
+
+// function checkString() {
+//   let reverseStr = input.value.split("").reverse().join("");
+
+//   if (reverseStr === input.value) {
+//     console.log("palindrome haa bhai");
+//     return;
+//   }
+//   console.log("palindrome nahi haa");
+// }
+
+
+// index of
+// includes
+
+
+
+// console.log(fruits.indexOf("watermelonss"));
+// console.log(fruits.includes("watermelon"));
+
+// let nestedArr = ['hello' , ['nested world' , ['double nested world']]]
+// console.log(nestedArr[1][1][0]);
+
+
+
+let cities = ['karachi' , 'lahore' , 'islamabad' , 'faislabad' , 'hyderabad' , 'sheikarpur' , 'quetta' , 'multan' , "sukkur" , "tandoallahyar" , "larkana"];
+
+// console.log("karachi" == "Karachi");
---
+++ 02-Javascript/06-Loops/app.js
@@ -0,0 +1,25 @@
+// console.log("hello worlds");
+
+// for loop
+// while loop
+// dowhile loop
+
+// initialization
+// condition check
+// increment
+
+
+// for (let i = 0; i < 10; i++) {
+//   console.log("hello world " , i);
+// }
+
+
+
+for (let i = 11; i <= 10; i++){
+    console.log("hello world " , i);
+}
+
+
+
+
+
