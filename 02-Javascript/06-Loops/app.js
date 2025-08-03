0b2b4b1224abbfb929bfe043c3f44c7d249f6ffc
mabdullahjs <mabdullah2037@outlook.com>
2025-08-03 17:42:19 +0500

    loops

--- 02-Javascript/06-Loops/app.js
+++ 02-Javascript/06-Loops/app.js
@@ -8,18 +8,234 @@
 // condition check
 // increment
 
-
 // for (let i = 0; i < 10; i++) {
 //   console.log("hello world " , i);
 // }
 
+// for (let i = 11; i <= 10; i++){
+//     console.log("hello world " , i);
+// }
+
+// iteration
+
+// for(let i = 0; i < 10; i++){
+//     console.log("hello world " , i);
+// }
+
+// console.log("console after loop");
+
+// table
+
+// let num = 8;
+
+// for(let i = 1; i <= 100; i++){
+//     console.log(num +  " * " + i + " = " + num * i);
+
+// }
+
+// for (let i = 1; i <= 10; i++) {
+//   console.log("table of  ==> ", i);
+
+//   for (let j = 1; j <= 10; j++) {
+//     // console.log(i + " * " + j + " = " + i * j);
+//     console.log(`${i} * ${j} = ${i * j}`);
+    
+//   }
+// }
 
 
-for (let i = 11; i <= 10; i++){
-    console.log("hello world " , i);
-}
+
+
+// let age = 29;
+
+// if(age >= 18){
+//     console.log("18 sa bara ha");
+    
+//     if(age > 22){
+//         console.log("22 sa bara ha");
+        
+//     }else {
+//         console.log("22 sa chota ha");
+        
+//     }
+// }else {
+//     console.log("18 sa chota ha");
+    
+// }
 
 
 
 
 
+
+
+
+// function outerFunc(){
+//     console.log("outer func");
+//     function innerFunc(){
+//         console.log("inner func");
+        
+//     }
+//     innerFunc()
+// }
+
+// outerFunc()
+
+
+
+
+
+
+
+// break | continue
+
+// for(let i = 1; i <= 5; i++){
+//     if(i == 5){
+//         break
+//     }
+//     console.log(i);   
+// }
+
+// for(let i = 0; i < 10; i++){
+//     if(i == 5){
+//         continue
+//     }
+//     if(i == 7){
+//         continue
+//     }
+//     console.log(i);
+    
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+// let fruits = ["apple" , "banana" , "orange" , "pineapple" , "watermelon" , "melon" , "mango" , "peach"];
+// const div = document.querySelector(".fruits");
+
+// // console.log(fruits);
+
+
+// for(let i = 0; i < fruits.length; i++){
+//     console.log(fruits[i]);
+//     div.innerHTML += "<p>" + fruits[i] + "</p>"
+    
+// }
+
+
+
+// let username = "muhammad abdullah khan"
+// let age = 21;
+// let email = "mabdullah@gmail.com"
+
+
+// console.log("username is " + username + " and age is " + age + " and email is  " + email);
+
+// console.log(`username is ${username} and age is ${age} and email is ${email}`);
+
+// console.log('asdad
+    
+//     ad');
+
+// console.log(`asdas
+    
+//     asdasd
+    
+    
+//     asd`);
+
+
+
+
+// const cities = [
+//   "Karachi",
+//   "Lahore",
+//   "Islamabad",
+//   "Rawalpindi",
+//   "Faisalabad",
+//   "Multan",
+//   "Peshawar",
+//   "Quetta",
+//   "Hyderabad",
+//   "Sialkot",
+//   "Gujranwala",
+//   "Bahawalpur",
+//   "Sargodha",
+//   "Abbottabad",
+//   "Sukkur",
+//   "Rahim Yar Khan",
+//   "Mardan",
+//   "Okara",
+//   "Dera Ghazi Khan",
+//   "Chiniot",
+//   "Jhelum",
+//   "Mingora",
+//   "Muzaffarabad",
+//   "Mirpur",
+//   "Khairpur",
+//   "Tando Adam",
+//   "Nawabshah",
+//   "Vehari",
+//   "Kotli",
+//   "Turbat",
+//   "Gojra",
+//   "Shikarpur",
+//   "Bhakkar",
+//   "Larkana",
+//   "Charsadda"
+// ];
+
+// let ol = document.querySelector("#cities");
+
+// for(let i = 0; i < cities.length; i++){
+//     console.log(cities[i]);
+//     ol.innerHTML += `<li>${cities[i]}</li>`
+// }
+
+
+
+// for loop
+// nested loop | nested if else | nested func
+// break | continue
+// array with loops
+// backtick ``
+
+
+
+
+
+
+// let i = 10;
+// while(i < 10){
+//     console.log(i);
+//     i++
+// }
+
+
+
+// let i = 10
+// do {
+//     console.log(i);
+//     i++
+    
+// } while (i < 10);
+
+
+// infinite loop
+
+// for(let i = 0; i < 10; i--)
+
+
+
+
+
+// todo app
+// loop array splice dom manipulation
