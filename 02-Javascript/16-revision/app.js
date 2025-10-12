36ae7cd36e3cfc00ea97244d1d713a02e216cd84
mabdullahjs <mabdullah2037@outlook.com>
2025-10-12 17:42:15 +0500

    single item updated

--- 02-Javascript/16-revision/app.js
+++ 02-Javascript/16-revision/app.js
@@ -32,12 +32,14 @@
 const div = document.querySelector(".container");
 const btnDiv = document.querySelector(".btns");
 
-const btnItems = []
+const btnItems = ["all posts"]
+let allPost;
 
 fetch('https://jsonplaceholder.typicode.com/posts')
     .then(res => res.json())
     .then(res => {
         console.log(res)
+        allPost = res
         res.map(item => {
             !btnItems.includes(item.userId) ? btnItems.push(item.userId) : null
             div.innerHTML += `
@@ -50,8 +52,8 @@ fetch('https://jsonplaceholder.typicode.com/posts')
         })
 
         // ya bilawaja lagaya ha is sa shyd code quality kahrab ho.
-        btnItems.map(item => {
-            btnDiv.innerHTML += `<button>userId ${item}</button>`
+        btnItems.map((item, index) => {
+            btnDiv.innerHTML += `<button onclick="testing(${index})">userId ${item}</button>`
         })
 
         console.log(btnItems);
@@ -64,8 +66,22 @@ fetch('https://jsonplaceholder.typicode.com/posts')
 
 
 
+function testing(index) {
+    div.innerHTML = ""
+    console.log(index + 1);
+    const filteredProduct = allPost.filter(item => item.userId === index + 1)
+    console.log(filteredProduct);
 
+    filteredProduct.map(item => {
+        div.innerHTML += `
+         <div class="children">
+            <h1>Title: ${item.title}</h1>
+            <p>body: ${item.body}</p>
+            <p>user id: ${item.userId}</p>
+        </div>
+        `
+    })
 
-
+}
 
 
