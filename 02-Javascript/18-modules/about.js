4d4abfe53139ac8cd467d522270ab75ed00b1cc5
mabdullahjs <mabdullah2037@outlook.com>
2025-10-19 17:29:46 +0500

    module in js

---
+++ 02-Javascript/18-modules/about.js
@@ -0,0 +1,6 @@
+// import { testing as abdullah , username } from "./app.js";
+// // import abdullah from "./app.js"
+
+// console.log(username);
+// abdullah()
+// // abdullah()
---
+++ 02-Javascript/18-modules/app.js
@@ -0,0 +1,10 @@
+function testing(){
+    console.log("testing from home page");
+    
+}
+
+let username = "abdullah"
+
+
+export {testing , username}
+// export default testing
